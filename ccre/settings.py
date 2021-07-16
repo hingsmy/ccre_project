b8025fc3d1a8c51012177064ddbf7d8de98d0c19
hingsmy <hingsmy.gmail.com>
2021-07-16 20:21:35 +0800

    Django Postgres Setup

--- ccre/settings.py
+++ ccre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'ccre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ccredb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
